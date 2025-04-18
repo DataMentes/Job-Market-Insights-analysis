6fa002ed778e2f1fcf1d79ee4eec26552018fcae
Abdallah mohamed <abdallah.m.rashad5@gmail.com>
2025-04-18 20:17:00 +0200

    add method to clean_data.py and do some cleaning in 02_cleaning_saudi.py

--- python_notebooks/02_cleaning_saudi.py
+++ python_notebooks/02_cleaning_saudi.py
@@ -1,2 +1,20 @@
-#%% md
-# Hello
+#%%
+import pandas as pd
+from scripts.clean_data import *
+#%%
+df = pd.read_csv('../data/raw/saudi-arabia_raw.csv')
+df.head(10)
+#%%
+df.tail(10)
+#%%
+df.info()
+#%%
+df.describe()
+#%%
+df.drop('Unnamed: 0', axis = 1, inplace=True)
+df
+#%%
+df.location.value_counts()
+#%%
+split_column(df, 'location', index=1, split_char='·', names='location')
+df
--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -1,7 +1,7 @@
 # Data cleaning functions here
 from deep_translator import GoogleTranslator
 from langdetect import detect
-
+import pandas as pd
 
 def translate_if_arabic(text, no_detect=False):
     if not text or not isinstance(text, str):
@@ -29,3 +29,17 @@ def apply_translation(data, column, rows='all'):
     else:
         for row in rows:
             data.at[row, column] = translate_if_arabic(data.at[row, column], no_detect=True)
+
+
+def split_column(df : pd.DataFrame, column, index, split_char , names, fill_value='Unknown'):
+    col = df[column]
+    result = pd.DataFrame(columns=names)
+    for row in col:
+        string_value = str(row)
+        split_list = string_value.split(split_char)
+        values = []
+        for i in index:
+            values.append(split_list[index].strip() if index < len(split_list) else fill_value)
+        pd.concat(result, values)
+
+    df = pd.merge(df, result, on=index)
