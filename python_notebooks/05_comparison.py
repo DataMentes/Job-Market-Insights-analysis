34f04d5954e25bef82f67ec93c19866c177a7ce5
Ali ahmed <146197555+ZeeeeCS@users.noreply.github.com>
2025-05-02 23:26:51 +0300

    Comparison Markdowns

--- python_notebooks/05_comparison.py
+++ python_notebooks/05_comparison.py
@@ -1,8 +1,12 @@
-#%%
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+
+
 # %% [markdown]
 # ## *Comparison Between Egpyt market and Saudi Arabia market*
 # ## *__________________________________________________________________________________________________________*
-#
+# 
 # %% [markdown]
 # ## Saudi Arabia 
 # ![alt text](5698b510-de4f-4f43-b800-9c1c58140e09.png)
@@ -18,3 +22,4 @@
 # ![image-2.png](attachment:image-2.png)
 # ## Egypt
 # ![alt text](ba1ac83a-211f-4f41-a483-a373deeb6363-1.jfif)
+
